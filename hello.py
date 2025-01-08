df00225736d2740bc478321919b101979e20a49f
vishaljoshi-git <joshivishal506@gmail.com>
2025-01-08 12:55:48 +0530

    New Branch

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 def greeting():
     print("Hello World. This is for simple git check")
 
-
+def add():
+    print("1+2= ", 1+2)
 def main():
     greeting()
+    add()
 
 if __name__ =="__main__":
     main()
